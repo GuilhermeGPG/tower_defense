bf4b8c02b2a3eb6c79a6ac8d7492dae4f6059b38
Guilherme Gomides <guilhermepgomides@gmail.com>
2024-07-05 18:47:35 -0300

    Add bevy_inspector_egui

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use bevy::{prelude::*, window::WindowResolution};
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 pub const HEIGHT: f32 = 720.0;
 pub const WIDTH: f32 = 1280.0;
 
 fn main() {
     App::new()
-        .insert_resource(ClearColor(Color::srgb(0.2, 0.2, 0.2)))
+        .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
         .add_systems(Startup, spawn_basic_scene)
         .add_systems(Startup, spawn_camera)
         .add_plugins(DefaultPlugins.set(WindowPlugin {
@@ -17,6 +18,7 @@ fn main() {
             }),
             ..Default::default()
         }))
+        .add_plugins(WorldInspectorPlugin::new())
         .run();
 }
 
@@ -27,18 +29,40 @@ fn spawn_camera(mut commands: Commands) {
     });
 }
 
-fn spawn_basic_scene(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
-    commands.spawn(
-        PbrBundle {
-            mesh:  meshes.add(Plane3d::new(Vec3::Y, Vec2::new(2.5, 2.5))),
-            material: materials.add(Color::srgb(0.3, 0.5, 0.3)),
+fn spawn_basic_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(shape::Plane {
+                size: 5.0,
+                subdivisions: 4,
+            }),
+            material: materials.add(Color::rgb(0.3, 0.5, 0.3)),
             ..Default::default()
-        }
-    );
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
-        material: materials.add(Color::srgb(0.67, 0.84, 0.92)),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..Default::default()
-    });
+        })
+        .insert(Name::new("Ground"));
+
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+            material: materials.add(Color::rgb(0.67, 0.84, 0.92)),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..Default::default()
+        })
+        .insert(Name::new("Tower"));
+
+    commands
+        .spawn(PointLightBundle {
+            point_light: PointLight {
+                intensity: 1500.0,
+                shadows_enabled: true,
+                ..default()
+            },
+            transform: Transform::from_xyz(4.0, 8.0, 4.0),
+            ..default()
+        })
+        .insert(Name::new("Light"));
 }
