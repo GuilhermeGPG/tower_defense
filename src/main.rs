91c6113b2146b0d0019f3522fd463235ac853981
Guilherme Gomides <guilhermepgomides@gmail.com>
2024-07-05 19:46:33 -0300

    Add bullet spawner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::f32::consts::PI;
+
 use bevy::{prelude::*, window::WindowResolution};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
@@ -7,8 +9,6 @@ pub const WIDTH: f32 = 1280.0;
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
-        .add_systems(Startup, spawn_basic_scene)
-        .add_systems(Startup, spawn_camera)
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Bevy Tower Defense".to_string(),
@@ -19,7 +19,20 @@ fn main() {
             ..Default::default()
         }))
         .add_plugins(WorldInspectorPlugin::new())
+        .register_type::<Tower>()
+        .register_type::<Lifetime>()
+        .add_systems(Startup, spawn_basic_scene)
+        .add_systems(Startup, spawn_camera)
+        .add_systems(Startup, asset_loading)
+        .add_systems(Update, tower_shooting)
+        .add_systems(Update, bullet_despawn)
         .run();
+}
+
+fn asset_loading(mut commands: Commands, assets: Res<AssetServer>) {
+    commands.insert_resource(GameAssets {
+        bullet_scene: assets.load("Bullet.glb#Scene0"),
+    });
 }
 
 fn spawn_camera(mut commands: Commands) {
@@ -52,12 +65,15 @@ fn spawn_basic_scene(
             transform: Transform::from_xyz(0.0, 0.5, 0.0),
             ..Default::default()
         })
-        .insert(Name::new("Tower"));
+        .insert(Name::new("Tower"))
+        .insert(Tower {
+            shotting_timer: Timer::from_seconds(1.0, TimerMode::Repeating),
+        });
 
     commands
         .spawn(PointLightBundle {
             point_light: PointLight {
-                intensity: 1500.0,
+                intensity: 1_500_000.0,
                 shadows_enabled: true,
                 ..default()
             },
@@ -65,4 +81,60 @@ fn spawn_basic_scene(
             ..default()
         })
         .insert(Name::new("Light"));
+}
+
+fn tower_shooting(
+    mut commands: Commands,
+    bullet_assets: Res<GameAssets>,
+    mut towers: Query<&mut Tower>,
+    time: Res<Time>,
+) {
+    for mut tower in &mut towers {
+        tower.shotting_timer.tick(time.delta());
+        if tower.shotting_timer.just_finished() {
+            let spawn_transform =
+                Transform::from_xyz(0.0, 0.7, 0.6).with_rotation(Quat::from_rotation_y(-PI / 2.0));
+
+            commands
+                .spawn(SceneBundle {
+                    scene: bullet_assets.bullet_scene.clone(),
+                    transform: spawn_transform,
+                    ..Default::default()
+                })
+                .insert(Lifetime {
+                    timer: Timer::from_seconds(0.5, TimerMode::Once),
+                })
+                .insert(Name::new("Bullet"));
+        }
+    }
+}
+
+fn bullet_despawn(
+    mut commands: Commands,
+    mut bullets: Query<(Entity, &mut Lifetime)>,
+    time: Res<Time>,
+) {
+    for (entity, mut lifetime) in &mut bullets {
+        lifetime.timer.tick(time.delta());
+        if lifetime.timer.just_finished() {
+            commands.entity(entity).despawn_recursive();
+        }
+    }
+}
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct Tower {
+    shotting_timer: Timer,
+}
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct Lifetime {
+    timer: Timer,
+}
+
+#[derive(Resource)]
+pub struct GameAssets {
+    bullet_scene: Handle<Scene>,
 }
